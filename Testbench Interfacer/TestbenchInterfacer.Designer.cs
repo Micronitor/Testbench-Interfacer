e5f01392f728f50e0379bcd448464d812fe3d80c
Lars Erik Songe Paulsen <lars.paulsen@micronitor.com>
2016-09-08 13:21:15 +0200

    Added "Toggle in on direction only" checkbox

--- Testbench Interfacer/TestbenchInterfacer.Designer.cs
+++ Testbench Interfacer/TestbenchInterfacer.Designer.cs
@@ -213,6 +213,8 @@
             this.Motor3_Position = new System.Windows.Forms.TextBox();
             this.panel5 = new System.Windows.Forms.Panel();
             this.lb_Micronitor = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cb_Toggle_in_on_direction = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -2155,12 +2157,34 @@
             this.lb_Micronitor.TabIndex = 94;
             this.lb_Micronitor.Text = "Micronitor 2016";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 95;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // cb_Toggle_in_on_direction
+            // 
+            this.cb_Toggle_in_on_direction.AutoSize = true;
+            this.cb_Toggle_in_on_direction.Location = new System.Drawing.Point(11, 842);
+            this.cb_Toggle_in_on_direction.Name = "cb_Toggle_in_on_direction";
+            this.cb_Toggle_in_on_direction.Size = new System.Drawing.Size(150, 17);
+            this.cb_Toggle_in_on_direction.TabIndex = 96;
+            this.cb_Toggle_in_on_direction.Text = "Toggle in on direction only";
+            this.cb_Toggle_in_on_direction.UseVisualStyleBackColor = true;
+            // 
             // Interfacer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(998, 936);
+            this.Controls.Add(this.cb_Toggle_in_on_direction);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.lb_Micronitor);
             this.Controls.Add(this.Motor3_Deacceleration);
             this.Controls.Add(this.Motor3_Distance);
@@ -2527,6 +2551,8 @@
         private System.Windows.Forms.TextBox Motor2_Velocityreal;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Label lb_Micronitor;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cb_Toggle_in_on_direction;
     }
 }
 
